4e3c50881da279d13578e8aefa344c52e469ae03
James Collins <collijk@uw.edu>
2018-07-26 10:22:55 -0700

    Fix the __about__.py

--- src/__about__.py
+++ src/__about__.py
@@ -1,6 +1,5 @@
 __all__ = [
-    "__title__", "__summary__", "__uri__", "__version__", "__author__",
-    "__email__", "__license__", "__copyright__",
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",  "__email__",
 ]
 
 __title__ = "gbd_mapping"
