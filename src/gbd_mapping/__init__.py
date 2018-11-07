b6c384ba0cc94f182bc74f2bfbf95ba5e47f2a3c
James Collins <collijk@uw.edu>
2018-11-07 08:52:31 -0800

    Rebuild etiologies mapping

--- src/gbd_mapping/__init__.py
+++ src/gbd_mapping/__init__.py
@@ -1,2 +1,3 @@
 from .id import meid, reiid, cid, sid, covid, hsid, scalar, UNKNOWN, UnknownEntityError
 from .base_template import GbdRecord, ModelableEntity, Restrictions, Tmred, Levels, ExposureParameters
+from .etiology import Etiology, etiologies
