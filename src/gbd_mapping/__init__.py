8f7a47e6c3ae2e4ec68fa84499d4bfff6916b732
James Collins <collijk@uw.edu>
2018-11-07 09:00:38 -0800

    Update sequela mapping

--- src/gbd_mapping/__init__.py
+++ src/gbd_mapping/__init__.py
@@ -1,3 +1,4 @@
 from .id import meid, reiid, cid, sid, covid, hsid, scalar, UNKNOWN, UnknownEntityError
 from .base_template import GbdRecord, ModelableEntity, Restrictions, Tmred, Levels, ExposureParameters
 from .etiology import Etiology, etiologies
+from .sequela import Sequela, Healthstate, sequelae
