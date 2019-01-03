1bd5771cb277e67241150b7e2725499637cffd8f
Kate Wilson <kwilson7@uw.edu>
2019-01-03 10:57:10 -0800

    switch violated_restrictions to tuple in order to be able to use text_wrap

--- src/gbd_mapping_generator/base_template_builder.py
+++ src/gbd_mapping_generator/base_template_builder.py
@@ -15,7 +15,7 @@ restrictions_attrs = (('male_only', 'bool'),
                       ('yll_age_group_id_end', 'int = None'),
                       ('yld_age_group_id_start', 'int = None'),
                       ('yld_age_group_id_end', 'int = None'),
-                      ('violated_restrictions', 'List=[]'),)
+                      ('violated_restrictions', 'Tuple = ()'),)
 tmred_attrs = (('distribution', 'str'),
                ('inverted', 'bool'), 
                ('min', 'scalar = None'),
