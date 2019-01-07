47b24821a51c7e6a40354237b6bdb68a67b9914c
James Collins <collijk@uw.edu>
2019-01-07 15:34:44 -0800

    Dont be recursive

--- src/gbd_mapping_generator/base_template_builder.py
+++ src/gbd_mapping_generator/base_template_builder.py
@@ -16,7 +16,7 @@ restrictions_attrs = (('male_only', 'bool'),
                       ('yld_age_group_id_start', 'int = None'),
                       ('yld_age_group_id_end', 'int = None'),)
 tmred_attrs = (('distribution', 'str'),
-               ('inverted', 'bool'), 
+               ('inverted', 'bool'),
                ('min', 'scalar = None'),
                ('max', 'scalar = None'),)
 categories_attrs = tuple([('cat1', 'str'), ('cat2', 'str')] + [(f'cat{i}', 'str = None') for i in range(3, 150)])
@@ -84,6 +84,10 @@ def make_gbd_record():
     def __iter__(self):
         for item in self.__slots__:
             yield getattr(self, item)
+            
+    def __eq__(self, other):
+        return all([getattr(self, item) == getattr(other, item) for item in self.__slots__
+                    if not isinstance(getattr(self, item), GbdRecord)])
 
     def __repr__(self):
         out = f'{self.__class__.__name__}('
