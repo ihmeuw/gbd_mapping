88e4a8f6a9a348fbb072e25efc17ca6ba4293cf4
Michelle Park <hpark3@uw.edu>
2019-01-08 12:47:50 -0800

    name fix

--- src/gbd_mapping_generator/covariate_builder.py
+++ src/gbd_mapping_generator/covariate_builder.py
@@ -15,7 +15,7 @@ def get_base_types():
                       ('by_sex', 'bool'),
                       ('dichotomous', 'bool'),
                       ('data_exist', 'bool'),
-                      ('low_value_exists', 'bool'),
+                      ('lower_value_exists', 'bool'),
                       ('upper_value_exists', 'bool'),
                       ('mean_value_exists', 'bool'),
                       ('sex_restriction_violated', 'Union[bool, None]'),
@@ -42,7 +42,7 @@ def make_covariate(name, covid, by_age, by_sex, dichotomous, data_exist, low_val
     out += TAB*2 + f"by_sex={bool(by_sex)},\n"
     out += TAB*2 + f"dichotomous={bool(dichotomous)},\n"
     out += TAB * 2 + f"data_exist={data_exist},\n"
-    out += TAB * 2 + f"low_value_exists={low_val_exist},\n"
+    out += TAB * 2 + f"lower_value_exists={low_val_exist},\n"
     out += TAB * 2 + f"upper_value_exists={upper_val_exist},\n"
     out += TAB * 2 + f"mean_value_exists={mean_exist},\n"
     out += TAB * 2 + f"sex_restriction_violated={sex_restriction},\n"
