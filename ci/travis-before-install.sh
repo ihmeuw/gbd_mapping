bab8e39d8a04c6c4dff4cad67a7f777b9d179ac5
Cody <chorst@uw.edu>
2019-05-07 11:56:15 -0700

    update before-install script to pull correct branches

--- ci/travis-before-install.sh
+++ ci/travis-before-install.sh
@@ -17,29 +17,27 @@ python -V
 
 pip install --upgrade pip setuptools
 
-if [ $TRAVIS_PULL_REQUEST = "false" ]
-then
-    branch=$TRAVIS_BRANCH
-else
-    branch=$TRAVIS_PULL_REQUEST_BRANCH
-fi
-
+# For push builds, TRAVIS_BRANCH is the branch name
+# For builds triggered by PR, TRAVIS_BRANCH is the branch targeted by the PR
+branch=$TRAVIS_BRANCH
 echo branch ${branch}
 
-
+# iterate over upstream dependencies
 for upstream_repo in vivarium vivarium_public_health
 do
+    upstream_branch_exists="$(git ls-remote --heads https://github.com/ihmeuw/${upstream_repo}.git ${branch})"
 
-    upstream_branch="$(git ls-remote --heads https://github.com/ihmeuw/${upstream_repo}.git ${branch})"
-
-    echo upstream branch found ${upstream_branch}
-
-    if [ "${upstream_branch}" ]
+    # if there is a match for upstream, use that
+    if [ ! -z "${upstream_branch_exists}" ]  # checks if not-empty
     then
+        echo upstream branch found for ${branch} in ${upstream_repo}
+        # clone and install upstream stuff if target branch exists
         git clone --branch=$branch https://github.com/ihmeuw/${upstream_repo}.git
         pushd ${upstream_repo}
         pip install .
         popd
+    else
+        echo no upstream branch found in ${upstream_repo}
     fi
 
 done
