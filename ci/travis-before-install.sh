031d4e1944f496b9e66263daeac8670ea24d65be
Cody <chorst@uw.edu>
2019-05-07 12:28:47 -0700

    remove erroneous upstream repo pulls

--- ci/travis-before-install.sh
+++ ci/travis-before-install.sh
@@ -17,31 +17,6 @@ python -V
 
 pip install --upgrade pip setuptools
 
-# For push builds, TRAVIS_BRANCH is the branch name
-# For builds triggered by PR, TRAVIS_BRANCH is the branch targeted by the PR
-branch=$TRAVIS_BRANCH
-echo branch ${branch}
-
-# iterate over upstream dependencies
-for upstream_repo in vivarium vivarium_public_health
-do
-    upstream_branch_exists="$(git ls-remote --heads https://github.com/ihmeuw/${upstream_repo}.git ${branch})"
-
-    # if there is a match for upstream, use that
-    if [ ! -z "${upstream_branch_exists}" ]  # checks if not-empty
-    then
-        echo upstream branch found for ${branch} in ${upstream_repo}
-        # clone and install upstream stuff if target branch exists
-        git clone --branch=$branch https://github.com/ihmeuw/${upstream_repo}.git
-        pushd ${upstream_repo}
-        pip install .
-        popd
-    else
-        echo no upstream branch found in ${upstream_repo}
-    fi
-
-done
-
+# gbd_mapping has no upstream github dependencies
 popd
-
 pip install .[test,docs]
