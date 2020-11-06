881c542c1acb14e27cf539928237f0eb46c51f19
KjellSwedin <kjells@uw.edu>
2020-11-06 13:59:36 -0800

    Updates for gbd 2019, add docstrings, typehints. Builds through id and base_template

--- src/gbd_mapping/__init__.py
+++ src/gbd_mapping/__init__.py
@@ -1,1 +1,3 @@
 from .id import me_id, rei_id, c_id, s_id, cov_id, hs_id, scalar, UNKNOWN, UnknownEntityError
+from .base_template import GbdRecord, ModelableEntity, Restrictions, Tmred, Categories
+from .cause import Cause, causes
