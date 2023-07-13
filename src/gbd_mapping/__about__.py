b1dfbb0878b10a6cd32e23da5d1b319ea027f0f2
Matthew Kappel <mkappel@uw.edu>
2023-07-13 13:22:38 -0700

    Use setuptools_scm, update CHANGELOG (#96)

--- src/gbd_mapping/__about__.py
+++ src/gbd_mapping/__about__.py
@@ -1,16 +1,17 @@
 __all__ = [
-    "__title__", "__summary__", "__uri__", "__version__", "__author__",  "__email__",
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__author__",
+    "__email__",
 ]
 
 __title__ = "gbd_mapping"
 __summary__ = "A programmatically accessible mapping of gbd entities."
 __uri__ = "https://github.com/ihmeuw/gbd_mapping"
 
-__version__ = "3.1.0"
-
 __author__ = "The vivarium developers"
 __email__ = "vivarium.dev@gmail.com"
 
 __license__ = "BSD-3-Clause"
-__copyright__ = f"Copyright 2022 {__author__}"
-
+__copyright__ = f"Copyright 2023 {__author__}"
--- src/gbd_mapping/__init__.py
+++ src/gbd_mapping/__init__.py
@@ -1,7 +1,18 @@
-from .id import me_id, rei_id, c_id, s_id, cov_id, hs_id, scalar, UNKNOWN, UnknownEntityError
-from .base_template import GbdRecord, ModelableEntity, Restrictions, Tmred, Categories
+from ._version import __version__
+from .base_template import Categories, GbdRecord, ModelableEntity, Restrictions, Tmred
 from .cause import Cause, causes
-from .sequela import Sequela, Healthstate, sequelae
-from .etiology import Etiology, etiologies
-from .risk_factor import RiskFactor, risk_factors
 from .covariate import Covariate, covariates
+from .etiology import Etiology, etiologies
+from .id import (
+    UNKNOWN,
+    UnknownEntityError,
+    c_id,
+    cov_id,
+    hs_id,
+    me_id,
+    rei_id,
+    s_id,
+    scalar,
+)
+from .risk_factor import RiskFactor, risk_factors
+from .sequela import Healthstate, Sequela, sequelae
