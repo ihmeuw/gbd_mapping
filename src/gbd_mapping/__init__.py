8ac9412472414d1b7fe758e3deb3e732a1635ccf
Hussain Jafari <hussainsjafari@gmail.com>
2024-11-05 14:43:49 -0800

    more linting

--- src/gbd_mapping/__init__.py
+++ src/gbd_mapping/__init__.py
@@ -1,9 +1,20 @@
 from ._version import __version__
-from .base_template import GbdRecord, ModelableEntity, Restrictions, Tmred, Categories
+from .base_template import Categories, GbdRecord, ModelableEntity, Restrictions, Tmred
 from .cause import Cause, causes
 from .covariate import Covariate, covariates
 from .etiology import Etiology, etiologies
-from .id import me_id, rei_id, c_id, s_id, cov_id, hs_id, scalar, UNKNOWN, UnknownEntityError
+from .id import (
+    UNKNOWN,
+    UnknownEntityError,
+    c_id,
+    cov_id,
+    hs_id,
+    me_id,
+    rei_id,
+    s_id,
+    scalar,
+)
+
     UNKNOWN,
     UnknownEntityError,
     c_id,
@@ -15,4 +26,4 @@ from .id import me_id, rei_id, c_id, s_id, cov_id, hs_id, scalar, UNKNOWN, Unkno
     scalar,
 )
 from .risk_factor import RiskFactor, risk_factors
-from .sequela import Sequela, Healthstate, sequelae
+from .sequela import Healthstate, Sequela, sequelae
